13010d394186a9a314d33f4518639a2b459531d4
ABIRAMI-487 <mabirami348@gmail.com>
2026-07-20 11:06:09 +0530

    Add files via upload

---
+++ Laststring.java
@@ -0,0 +1,9 @@
+public class Laststring {
+    public int lengthOfLastWord(String s)
+    {
+        s = s.trim();
+        String[] words = s.split(" ");
+        return words[words.length-1].length();
+        
+    }
+}
